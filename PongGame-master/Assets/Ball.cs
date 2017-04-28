61da340df84fc274f2624a30767670d1c7223037
Nathalie Goh-Livorness <nalivorn@microsoft.com>
2017-04-28 04:20:58 -0700

    Sample Pong Game
    This is a pong game that can be used to sample Nether.

---
+++ PongGame-master/Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    float sx;
+    float sy;
+
+	// Use this for initialization
+	void Start () {
+        sx = Random.Range(0, 2) == 0 ? -1 : 1;
+        sy = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(5, 10) * sx, Random.Range(5, 10) * sy, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ PongGame-master/Assets/Control.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = .2f;
+
+        if (gameObject.name == "Bump1")
+            transform.Translate(0, Input.GetAxis("Horizontal") * speed, 0);
+        else
+            transform.Translate(0, Input.GetAxis("Vertical") * speed, 0);
+          
+
+	}
+}
